3ccf7470980e43fb9c482f351f547edc02f42cef
Jinchun Xia <xiajinchunc@gmail.com>
2015-04-19 18:37:56 +0800

    Add L05SuperCMethod

---
+++ L05SuperCMethod/L05SuperCMethod/Man.cpp
@@ -0,0 +1,9 @@
+//
+//  Man.cpp
+//  L03Classes
+//
+//  Created by Jinchun Xia on 15/4/19.
+//  Copyright (c) 2015年 TEAM. All rights reserved.
+//
+
+#include "Man.h"
---
+++ L05SuperCMethod/L05SuperCMethod/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  L05SuperCMethod
+//
+//  Created by Jinchun Xia on 15/4/19.
+//  Copyright (c) 2015年 TEAM. All rights reserved.
+//
+
+#include <iostream>
+#include "Man.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
